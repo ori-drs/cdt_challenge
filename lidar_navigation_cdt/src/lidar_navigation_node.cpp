a012ac3ebbafe9df66e673ef3b8bc06b6c1fe8db
YiduoWang47 <ywang@oxfordrobotics.institute>
2020-03-04 13:24:06 +0000

    Change file name to be more explanatory/consistent

--- lidar_navigation_cdt/src/lidar_navigation_node.cpp
+++ lidar_navigation_cdt/src/lidar_navigation_node.cpp
@@ -1,9 +1,9 @@
-#include "grid_map_cdt/Challenge.hpp"
+#include "grid_map_cdt/lidar_navigation.hpp"
 #include <ros/ros.h>
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "grid_map_filters_demo");
+  ros::init(argc, argv, "lidar_navigation_node");
   ros::NodeHandle nodeHandle("~");
   bool success;
   grid_map_demos::NavigationDemo navigationDemo(nodeHandle, success);
